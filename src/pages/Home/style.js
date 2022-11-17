f4b84cb7e49a2bd7dbe79090a29be8678e25eed3
Guilherme Gavassa <guilhermegavassa.tecn@gmail.com>
2022-11-17 02:35:49 -0300

    primeiro commit

---
+++ src/pages/Home/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #7B8AFB;
+  color: #f4f4f4;
+  height: 100vh;
+  gap: 20px;
+`;
+
+export const Title = styled.h2``;
